53f0ba1c832797dfab31cafd7b4d495159d8e379
pranavj7Z <pranavjayaraj7@gmail.com>
2019-09-01 22:37:15 +0530

    update release apk

--- app/src/main/java/com/pranavjayaraj/basis/ConnectionDetector.kt
+++ app/src/main/java/com/pranavjayaraj/basis/ConnectionDetector.kt
@@ -5,9 +5,6 @@ import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 
-/**
- * Created by ALMIGHTY on 3/6/2017.
- */
 class ConnectionDetector {
 
 
